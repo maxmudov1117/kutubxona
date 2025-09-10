c9a055ea9cf09927293d1b0ca635f0308ab137ea
maxmudov1117 <bekmurod20051117@gmail.com>
2025-09-10 10:33:59 +0500

    delete search

--- core/urls.py
+++ core/urls.py
@@ -10,9 +10,13 @@ urlpatterns = [
     path('', home_page),
     path('muallif/', muallif_list),
     path("muallif/<int:muallif_id>/",tanlangan_view ),
+    path("muallif/<int:pk>/delete/confirm/",muallif_confirm_delete_view ),
+    path("muallif/<int:pk>/delete/",muallif_delete ),
     path('kitob/',kitob_view),
     path("kitob/<int:kitob_id>/",tanlangan_kitob_view, name="tanlangan_kitob"),
     path("record/", record_view),
+    path("record/<int:pk>/delete/confirm/", record_delete_confirm_view),
+    path("record/<int:pk>/delete/", record_delete_view),
     path("tirik/", tirik_view),
     path("order/<int:record_id>/",tanlangan_record_view, name="tanlangan_record_view")
 ]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from django.http import HttpResponse
 from .models import *
@@ -16,13 +16,33 @@ def muallif_list(request):
     eng_katta = Muallif.objects.order_by("-kitob_soni")[:3]
     katta_yosh = Muallif.objects.order_by("tugilgan_sana")[:3]
     eng_kop_kitob = Muallif.objects.filter(kitob_soni__lt=10)
+    search = request.GET.get('search')
+
+    if search:
+        mualliflar = mualliflar.filter(ism__contains=search)
+
     context = {
         'mualliflar' : mualliflar,
         'eng_katta' : eng_katta,
         'katta_yosh' : katta_yosh,
         'eng_kop_kitob' : eng_kop_kitob,
+        'search':search,
     }
     return render(request,"muallif.html", context=context)
+
+def muallif_confirm_delete_view(request, pk):
+    muallif = get_object_or_404(Muallif, pk=pk)
+    context = {
+        'muallif':muallif,
+    }
+    return render(request, "muallif_confirm_delete.html", context=context)
+
+
+def muallif_delete(request,pk):
+    muallif = get_object_or_404(Muallif,pk=pk)
+    muallif.delete()
+    return redirect("/muallif/")
+
 
 def tanlangan_view(request, muallif_id):
     muallif = Muallif.objects.get(id=muallif_id)
@@ -60,12 +80,31 @@ def record_view(request):
     recordlar = Record.objects.all()
     record_sana = Record.objects.order_by("-olingan_sana")[:3]
     bitiruvchilar = Record.objects.filter(talaba__kurs=4)
+    search = request.GET.get('search')
+
+    if search:
+        recordlar = recordlar.filter(talaba__ism__contains=search)
+
     context = {
         "recordlar":recordlar,
         'record_sana':record_sana,
         'bitiruvchilar' : bitiruvchilar,
+        'search':search,
     }
     return render(request,"record.html", context = context)
+
+def record_delete_confirm_view(request,pk):
+    record = get_object_or_404(Record,pk=pk)
+    context = {
+        "record":record,
+    }
+    return render(request, "record_delete_confirm.html", context=context)
+
+
+def record_delete_view(request,pk):
+    record = get_object_or_404(Record,pk=pk)
+    record.delete()
+    return redirect("/record/")
 
 def tirik_view(request):
     tiriklar = Muallif.objects.filter(tirik=True)
